85c3a228adb7ff585a0d7d508391c98b509c20f8
T4c0 <julianv483@gmail.com>
2023-10-30 19:09:18 -0500

    Update controller.js

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,4 @@
+const { userGoals } = require("./userGoals");
 module.exports = {
   getCompliment: (req, res) => {
     const compliments = [
@@ -28,20 +29,19 @@ module.exports = {
     res.status(200).send(randomFortune);
   },
   getGoals: (req, res) => {
-    const userGoals = ["Graduate", "Find a job"];
     res.status(200).send(userGoals);
   },
-  updateGoal: (req, res) => {
-    const goalId = req.params.id;
-    const updatedGoal = req.body.updatedGoal;
-
-    // Find and update the goal with the given ID
-    const goalToUpdate = userGoals.find((goal) => goal.id === goalId);
-    if (goalToUpdate) {
-      goalToUpdate.goal = updatedGoal;
-      res.status(200).json({ message: "Goal updated successfully" });
+  addGoals: (req, res) => {
+    const { text } = req.body;
+  
+    if (text) {
+      const newGoal = {
+        id: userGoals.length + 1, // Assign a unique ID
+        text,
+      };
+      userGoals.push(newGoal);
+      res.json({ success: true, goal: newGoal });
     } else {
-
       res.status(400).json({ error: "Invalid goal data" });
     }
   },
@@ -60,8 +60,6 @@ module.exports = {
       const deletedGoal = userGoals.splice(goalIndex, 1)[0];
 
       res.json({ success: true, deletedGoal });
-=======
-      
     }
   },
 };
